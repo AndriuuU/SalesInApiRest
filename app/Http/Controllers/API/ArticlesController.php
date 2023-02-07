93e405cfdbf7f9d7022698961cb337b0651de659
Andres Molina <66785221+AndriuuU@users.noreply.github.com>
2023-02-07 08:10:52 +0100

    Api Articles

--- app/Http/Controllers/API/ArticlesController.php
+++ app/Http/Controllers/API/ArticlesController.php
@@ -1,86 +1,73 @@
 <?php
 
 namespace App\Http\Controllers\API;
-
+use App\Articles;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class ArticlesController extends Controller
 {
     public $successStatus = 200;
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
+
+    public function index() {
+        $articles = Articles::all();
+
+        return response()->json(['Articulos' => $articles->toArray()], $this->successStatus);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+    public function store(Request $request) {
+        $input = $request->all();
+
+        $validatedData = $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'cicle_id' => 'required',
+        ]);
+    
+
+        if($validator->fails()){
+            return response()->json(['error' => $validator->errors()], 401);       
+        }
+
+        $article = Articles::create($input);
+
+        return response()->json(['Articulos' => $article->toArray()], $this->successStatus);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+    public function show($id) {
+        $article = Articles::find($id);
+
+        if (is_null($article)) {
+            return response()->json(['error' => $validator->errors()], 401);
+        }
+
+        return response()->json(['Articulos' => $article->toArray()], $this->successStatus);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
+    public function update(Request $request, Articles $article) {
+        $input = $request->all();
+
+        $validatedData = $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'cicle_id' => 'required'
+        ]);
+
+        if($validator->fails()){
+            return response()->json(['error' => $validator->errors()], 401);       
+        }
+
+        $article->title = $input['title'];
+        $article->description = $input['description'];
+        $article->cicle_id = $input['cicle_id'];
+        $article->save();
+
+        return response()->json(['Articulos' => $article->toArray()], $this->successStatus);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+    public function destroy(Articles $article) {
+        $article->delete();
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        return response()->json(['Articulos' => $article->toArray()], $this->successStatus);
     }
 }
